3c6ab68b09853b6ead5f0facef5ff5c72f4ff60f
SammyMusyoki <sammerkhan328@gmail.com>
2024-06-13 09:30:30 +0300

    slices & arrays

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Cannot declare nameFive := "Cannot be declared here"
 
@@ -42,6 +45,8 @@ func main() {
 	// When using uint - cannot use negative integers
 	var numThree uint64 = 12423423232324234234
 
+	fmt.Println(numOne, numTwo, numThree)
+
 	// float
 	var scoreOne float32 = 25.89
 	var scoreTwo float64 = 4343493434343453.6
@@ -64,6 +69,7 @@ func main() {
 
 	age := 22
 	name := "Sammy"
+	points := 254.555
 
 	// Println
 	// Automatically inserts a new line
@@ -72,9 +78,74 @@ func main() {
 	fmt.Println("My age is", age, "and my name is", name)
 
 	// Printf (formatted strings) %_ = format specifier
-	// Does not add new lines automatically
-	// %v is for variable
-	// %q is quote
+	/* 
+		Does not add new lines automatically
+		%v is variable
+		%q is quotes = the variables will be in quotes in they are a string  "Sammy" and in bits "\x16" if it an integer
+		%T is Type =  used to return the type of a variable if its an integer(25) will return int if string("sammy") will return string
+		%f if float = returns the float value
+		%0.1f will return float in 1 decimal point if %0.2f will return 2 dp
+	 */
 	fmt.Printf("my age is %v and my name is %v \n", age, name)
 	fmt.Printf("my age is %q and my name is %q \n", age, name)
+	fmt.Printf("age is of type %T \n", age)
+	fmt.Printf("you scored %f \n", points)
+	fmt.Printf("you scored %0.2f \n", points)
+
+	// Sprintf (save formatted strings)
+	var str = fmt.Sprintf("my age is %v and my name is %v \n", age, name)
+	fmt.Println("the saved string is:", str)
+
+	// Arrays and Slices
+	// Cannot append to arrays but can append in slices.
+	// var ages [3]int = [3]int{20, 24, 30}
+	// Can also be written this way
+	var ages = [3]int{20, 24, 30}
+
+	names := [4]string{
+		"Babypluto",
+		"Klaus",
+		"Slice",
+		"Kenny",
+	}
+
+	// Change a string of index[i] in names
+	names[1] = "BonBon"
+
+	fmt.Println(ages, len(ages))
+	fmt.Println(names, len(names))
+
+	// Slices (use arrays under the hood but can be modified)
+	var scores = []int{
+		100, 50, 65,
+	}
+	scores[2] = 25
+
+	// Append in slices
+	scores = append(scores, 98)
+
+	fmt.Println(scores, len(scores))
+
+	// Slice ranges
+	rangeOne := names[1:3]
+	rangeTwo := names[2:]
+	rangeThree := names[:3]
+
+	fmt.Println(rangeOne, rangeTwo, rangeThree)
+	
+	// can also be appended
+	rangeOne = append(rangeOne, "Freddy")
+
+	fmt.Println(rangeOne, rangeTwo, rangeThree)
+
+
+	// STANDARD LIBRARY
+
+
+}
+
+func greeting() {
+	greeting := "Hello there friends!"
+
+	fmt.Println(strings.Contains(greeting, "hello"))
 }
